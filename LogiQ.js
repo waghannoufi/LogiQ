93315d716b949704816522beedb63684bd7753f4
Lotfi_Bengrad <lbengrad@student.che.nl>
2021-01-12 19:04:49 +0100

    no message

--- LogiQ.js
+++ LogiQ.js
@@ -1,43 +1,37 @@
 $(function () {
-    $("#canvas .items").sortable({
-            connectWith: "ul"  
-    });
-     
-    $("ul[id^='control']").draggable({
-        helper: "clone",
-        connectToSortable: ".items"
-    });
+  $("#canvas .items").sortable({
+          connectWith: "ul"
+  });
+
+  $("ul[id^='control']").draggable({
+      helper: "clone",
+      connectToSortable: ".items"
+  });
 });
 
 
 
-         $(document).ready(function(){
-                    $("#reset").click(function(){
-                      $("#canvas").empty();
-                    });
-                  });    
- 
+       $(document).ready(function(){
+                  $("#reset").click(function(){
+                    $("#canvas").empty();
+                  });
+                });
 
 
 
-                      function EigenschappenFunc() {
-                        var person = {
-                            eigenschap1 : document.getElementById('fname').value,
-                            eigenschap2 : document.getElementById('lname').value
 
-                        };
-                        localStorage.setItem("eigenschap", JSON.stringify(person));
+                    function EigenschappenFunc() {
+                      var person = {
+                          eigenschap1 : document.getElementById('fname').value,
+                          eigenschap2 : document.getElementById('lname').value
+
+                      };
+                      localStorage.setItem("eigenschap", JSON.stringify(person));
+                      }
+
+                      function EigenschappenInladen() {
+                          var a = JSON.parse(localStorage.getItem('eigenschap'));
+
+                          document.getElementById("eigenschappen").innerHTML = a.eigenschap1 + "  "+ a.eigenschap2;
                         }
-
-                        function EigenschappenInladen() {
-                            var a = JSON.parse(localStorage.getItem('eigenschap'));
-    
-                            document.getElementById("eigenschappen").innerHTML = a.eigenschap1 + "  "+ a.eigenschap2;
-                          }
-                    
-
-                          
-                      
-                      
-                      
                   
